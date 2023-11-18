08b64ee8e0822d2e9a3390f2df52ad0b4a2c4c95
Mohamedswahed <mohamedswahed@gmail.com>
2023-11-18 07:14:16 -0800

    insertion_sort_list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,48 +1,57 @@
 #include "sort.h"
-
 /**
- * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
- * @h: A pointer to the head of the doubly-linked list.
- * @n1: A pointer to the first node to swap.
- * @n2: The second node to swap.
+ * swap_lst - function to swap two nodes in the list
+ * @x:  adress of the first node
+ * Return: void
  */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+void swap_lst(listint_t *x, listint_t *y)
 {
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
-	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
-}
+	if (x->prev)
+	{
+		(x->prev)->next = y;
+	}
+	if (y->next)
+	{
+		(y->next)->prev = x;
+	}
+	x->next = y->next;
+	y->prev = x->prev;
+	x->prev = y;
+	y->next = x;
 
+}
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers
- *                       using the insertion sort algorithm.
- * @list: A pointer to the head of a doubly-linked list of integers.
- *
- * Description: Prints the list after each swap.
+ * insertion_sort_list -  using the Insertion sort algorithm
+ * sorts a doubly linked list of integers in ascending order
+ * @list: list to be sorted doubly linked list
+ * Return: void
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iter, *insert, *tmp;
+	listint_t *i, *j;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	i = (*list)->next;
+	while (i)
 	{
-		tmp = iter->next;
-		insert = iter->prev;
-		while (insert != NULL && iter->n < insert->n)
+		j = i;
+		i = i->next;
+		while (j && j->prev)
 		{
-			swap_nodes(list, &insert, iter);
-			print_list((const listint_t *)*list);
+			if (j->prev->n > j->n)
+			{
+				swap_lst(j->prev, j);
+				if (!j->prev)
+					*list = j;
+				print_list((const listint_t *)*list);
+			}
+			else
+				j = j->prev;
 		}
+
 	}
+
 }
+
